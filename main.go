3e179ed86121aeb46e7730f294a339dbb8b5e3cd
Aman Chopra <amanchopra.atg@gmail.com>
2024-07-20 16:30:07 +0530

    a basic structure of web server

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main()  {
 	fmt.Println("A Sample Web Server with Redis Caching")
+
+	http.HandleFunc("/api/devrelData", getData)
+	http.ListenAndServe(":8080", nil)
+}
+
+func getData(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w, "Hello fellow DevRel's")
 }
