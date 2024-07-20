f27daf9a6fe1d1c695b315607125008ceb862b8b
Aman Chopra <amanchopra.atg@gmail.com>
2024-07-20 16:57:21 +0530

    installed redis package and est conn with redis

--- main.go
+++ main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+
+	"github.com/redis/go-redis/v9"
 )
+
+var ctxBg = context.Background()
+var redisClient *redis.Client;
 
 func main()  {
 	fmt.Println("A Sample Web Server with Redis Caching")
 
-	http.HandleFunc("/api/devrelData", getData)
-	http.ListenAndServe(":8080", nil)
+	// initialize the redis client
+	redisClient = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
+	})
+
+	http.HandleFunc("/api/devrelData", getData) //defining the api endpoint
+	http.ListenAndServe(":8080", nil) //defining the port
 }
 
 func getData(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprintf(w, "Hello fellow DevRel's")
+
+	// code to fetch the data
 }
